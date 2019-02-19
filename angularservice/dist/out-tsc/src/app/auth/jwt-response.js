e93351d7c3ea3c877897e8de851dbb9a124a8623
amit-s-k <amitsk2014@gmail.com>
2019-02-19 17:11:12 +0530

    dist files

--- angularservice/dist/out-tsc/src/app/auth/jwt-response.js
+++
@@ -1,7 +0,0 @@
-var JwtResponse = /** @class */ (function () {
-    function JwtResponse() {
-    }
-    return JwtResponse;
-}());
-export { JwtResponse };
-//# sourceMappingURL=jwt-response.js.map
--- angularservice/dist/out-tsc/src/app/auth/login-info.js
+++
@@ -1,9 +0,0 @@
-var AuthLoginInfo = /** @class */ (function () {
-    function AuthLoginInfo(email, password) {
-        this.email = email;
-        this.password = password;
-    }
-    return AuthLoginInfo;
-}());
-export { AuthLoginInfo };
-//# sourceMappingURL=login-info.js.map
